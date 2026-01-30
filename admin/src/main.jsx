1cf2d60c75721ea12d26058d21a3af379b7b690f
MesBoisAdmin <amcbunq@amccredit.com>
2026-01-30 22:08:42 +0000

    Update application

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+if (import.meta.env.PROD) {
+  console.log = () => { };
+  console.info = () => { };
+  console.warn = () => { };
+  // On garde console.error pour les vrais crashs
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
